2f55801767710f15658f33c1ab925483d5a212f3
Nathan McKinley <ndm25@case.edu>
2012-12-02 23:19:06 -0500

    working fill_db and models
    Signed-off-by: Nathan McKinley <ndm25@case.edu>

--- django/scheduler/course_scheduler/models.py
+++ django/scheduler/course_scheduler/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class Class(models.Model):
     class_number = models.IntegerField()
     dept = models.CharField(max_length=10)
-    classname = models.CharField(max_length=120)
-    instructor = models.ForeignKey('Instructor')
+    classname = models.CharField(max_length=350)
     description = models.CharField(max_length = 4096)
 
 class MeetingTime(models.Model):
@@ -13,14 +12,18 @@ class MeetingTime(models.Model):
     meeting_end = models.TimeField()
     meeting_date = models.DateField()
     meeting_end_date = models.DateField()
-    meeting_recur_type = models.CharField(max_length=7)
+    meeting_recur_type = models.CharField(max_length=12)
     meeting_location = models.CharField(max_length=50)
 
 
 class Instructor(models.Model):
     email = models.CharField(max_length=10)
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 50)
     office = models.CharField(max_length = 15)
+
+class Instructs(models.Model):
+    instructor = models.ForeignKey('Instructor')
+    meeting = models.ForeignKey('MeetingTime')
 
 class Student(models.Model):
     case_id = models.CharField(max_length=6)
--- django/scheduler/settings.py
+++ django/scheduler/settings.py
@@ -115,7 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'course_scheduler'    
+    'scheduler.course_scheduler'    
 # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
