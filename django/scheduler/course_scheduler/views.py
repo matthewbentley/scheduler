47ff8d65ee1859dff94fe144a5eb3b906dac5eea
Stuart Long <srl51@case.edu>
2012-12-03 17:52:25 -0500

    bug fix

--- django/scheduler/course_scheduler/views.py
+++ django/scheduler/course_scheduler/views.py
@@ -2,18 +2,19 @@
 from django.shortcuts import render
 from django import forms
 from course_scheduler.models import Class
+from django.http import Http404
 
 def schedule(request):
     return render(request, 'schedule.html')
 
 def add(request):
-    classes = {len(Class.objects.all())}
     if request.method == 'GET':
         criterion = request.GET.get('Search', None)
-        classes = Class.objects.filter(classname__contains=criterion)
-        #classes = Class.objects.all()
+        #classes = Class.objects.filter(classname__contains=criterion)
+        classes = Class.objects.all()
         
-    return render(request, 'add.html', {'crit' : request.GET.get('Search', None), "classes" : classes})
+        return render(request, 'add.html', {'crit' : request.GET.get('Search', None), "classes" : classes})
+    raise Http404
 
 def info(request):
     course = request.GET.get('course', NONE)
