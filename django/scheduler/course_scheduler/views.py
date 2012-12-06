2c1f786098ddc3e3995f8ec94c6cbe8884fc09ba
Stuart Long <illusivestu@gmail.com>
2012-12-05 23:39:51 -0500

    Jason: Fixed colors on views.py...again
    Signed-off-by: Stuart Long <illusivestu@gmail.com>

--- django/scheduler/course_scheduler/views.py
+++ django/scheduler/course_scheduler/views.py
@@ -44,7 +44,7 @@ def schedule(request):
             height *= 1.2
             height += 3
             if len(colors_temp) == 0:
-                colors_temp = list(colors)
+                color_temp = list(colors)
             randColor = random.randint(0, len(color_temp)-1)
             color = color_temp[randColor] + ''
             del color_temp[randColor]
