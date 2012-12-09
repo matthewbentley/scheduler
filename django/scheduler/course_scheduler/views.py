698c4424ecd87709489d4e268b8f708f80a75c5d
Stuart Long <srl51@case.edu>
2012-12-09 15:35:17 -0500

    Typo

--- django/scheduler/course_scheduler/views.py
+++ django/scheduler/course_scheduler/views.py
@@ -119,7 +119,7 @@ def add(request):
         #TODO better regexes
         #patt = re.compile('(\w\w\w\w ((\w\w\w)|(\w\w\w\w)))|(\w\w\w\w\w\w\w)')
         patt = re.compile('(\w\w\w\w( )*(\d+|(\d+w)))')
-        if form.isValid():
+        if form.is_Valid():
             criterion = form.cleaned_data['criterion']
             if patt.match(criterion):
                 str = string.replace(criterion, ' ', '')
