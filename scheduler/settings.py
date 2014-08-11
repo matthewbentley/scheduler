291cca814c6038cb895000b679552b2aefc6e85e
Stuart Long <stuart.richard.long@gmail.com>
2014-08-10 21:52:27 -0700

    Revert "added debug toolbar to insalled apps"
    This reverts commit c1fb8756d990484541d3cbcac4a8bad7cb8c5cc1.

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -130,7 +130,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'course_scheduler',
-    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
