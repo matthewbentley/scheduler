98dfcaca835c3114873ba60a7641209f9991ed1a
Stuart Long <stuart.richard.long@gmail.com>
2014-08-10 23:24:29 -0700

    package test

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 import course_scheduler
-from course_scheduler.strings import Strings
+from scheduler.course_scheduler.strings import Strings
 
 sys.path.append('/srv/www/scheduler2/application')
 sys.path.append('/srv/www/scheduler2/application/scheduler')
