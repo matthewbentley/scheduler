3fd2469aa556598500d79f9a7913862395c6a1b9
Stuart Long <stuart.richard.long@gmail.com>
2014-08-10 23:10:25 -0700

    package test

--- scheduler/course_scheduler/views.py
+++ scheduler/course_scheduler/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
 import course_scheduler
-import course_scheduler.scheduler_strings
 from course_scheduler.models import *
 from course_scheduler.scheduler_strings import *
 from django.http import Http404
